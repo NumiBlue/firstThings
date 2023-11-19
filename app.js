d7e53ebda6414c42fb1f59e225087bce55b57c97
NumiBlue <thekindlyblog@gmail.com>
2023-11-19 10:40:16 -0500

    changes css, js

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Replace "YOUR_UNSPLASH_IMAGE_URL" with the actual URL of your Unsplash image
+    const imageUrl = "‘https://unsplash.com/photos/a-black-and-white-photo-of-a-pattern-9C59SQb2ps0?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash";
+    
+    document.body.style.backgroundImage = `url(${imageUrl})`;
+  });
