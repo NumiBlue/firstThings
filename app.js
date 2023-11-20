0454394405240976f72946cebe656ae887221dc4
NumiBlue <thekindlyblog@gmail.com>
2023-11-20 15:40:54 -0500

    adds secondary background color

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Replace with the actual URL of your Unsplash image
+    // Displays Unsplash image
     const imageUrl = "https://source.unsplash.com/9C59SQb2ps0";
 
     document.body.style.backgroundImage = `url(${imageUrl})`;
